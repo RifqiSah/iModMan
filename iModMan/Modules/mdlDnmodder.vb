9193eaeee21054975a2286612c827b874ccddd21
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-19 17:05:57 +0700

    add dnmodder support

---
+++ iModMan/Modules/mdlDnmodder.vb
@@ -0,0 +1,11 @@
+﻿Imports System.IO
+
+Module mdlDnmodder
+    Dim DNModderFolder As String = Application.StartupPath & "\DNModder"
+    Dim DNModderDll1 As String = Application.StartupPath & "\ddraw.dll"
+    Dim DNModderDll2 As String = Application.StartupPath & "\bdvid32_2.dll"
+
+    Public Function isDNModderExists()
+        Return Directory.Exists(DNModderFolder) And File.Exists(DNModderDll1) And File.Exists(DNModderDll2)
+    End Function
+End Module
