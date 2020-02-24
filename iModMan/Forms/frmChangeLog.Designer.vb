ae297323b9181d9fc344bf8317d53e7314e7bf27
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-24 08:15:53 +0700

    forgot to add 1 file on changelog information

---
+++ iModMan/Forms/frmChangeLog.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmChangeLog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstCommit = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'lstCommit
+        '
+        Me.lstCommit.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.lstCommit.FullRowSelect = True
+        Me.lstCommit.GridLines = True
+        Me.lstCommit.HideSelection = False
+        Me.lstCommit.Location = New System.Drawing.Point(12, 12)
+        Me.lstCommit.Name = "lstCommit"
+        Me.lstCommit.Size = New System.Drawing.Size(537, 200)
+        Me.lstCommit.TabIndex = 8
+        Me.lstCommit.UseCompatibleStateImageBehavior = False
+        Me.lstCommit.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Date"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Author"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Message"
+        Me.ColumnHeader3.Width = 121
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Commit Hash"
+        Me.ColumnHeader4.Width = 235
+        '
+        'frmChangeLog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(559, 224)
+        Me.Controls.Add(Me.lstCommit)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmChangeLog"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Change Log"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstCommit As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+End Class
