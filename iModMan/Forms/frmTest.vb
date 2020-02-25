67d21383014f3ce595596f2b3bdec290fbc6786c
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-25 18:57:19 +0700

    pak file test using temp file

--- iModMan/Forms/frmTest.vb
+++ iModMan/Forms/frmTest.vb
@@ -17,7 +17,7 @@ Public Class frmTest
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        DnPakPack("", "G:\iModMan\bin\temp\test.pak")
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
