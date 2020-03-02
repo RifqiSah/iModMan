8e4aa7da0dd3b0830b08f564e081e331dc902e2f
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-02 12:42:40 +0700

    code refactoring

--- iModMan/Modules/mdlExec.vb
+++ iModMan/Modules/mdlExec.vb
@@ -1,10 +1,11 @@
 ﻿Imports System.IO
 
 Module mdlExec
-    Public Sub execFile(sPath As String)
+    Public Sub execFile(sPath As String, sParam As String)
         Dim psi As New ProcessStartInfo
 
         psi.FileName = sPath
+        psi.Arguments = sParam
         psi.Verb = "runas"
         psi.WorkingDirectory = Path.GetDirectoryName(sPath)
         psi.UseShellExecute = False
