e6fab48adba8f915811bb2d109e9cc28fa76b1f7
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-06 11:36:05 +0700

    refactor: running dragonnest without vanilla launcher

--- iModMan/Modules/mdlDragonNest.vb
+++ iModMan/Modules/mdlDragonNest.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
+Imports System.Xml
 
 Module mdlDragonNest
+    Dim DNPatchConfig As String = "http://patchsea.dragonnest.com/Game/DragonNest/Patch/PatchConfigList.xml"
     Dim DNFolder As String = Path.Combine(Application.StartupPath, "XIGNCODE")
     Dim DNExe As String = Path.Combine(Application.StartupPath, "DragonNest.exe")
     Dim DNLauncherExe As String = Path.Combine(Application.StartupPath, "dnlauncher.exe")
@@ -25,6 +27,38 @@ Module mdlDragonNest
     End Function
 
     Public Sub runDragonNest()
+        Dim ip As String = Nothing
+        Dim port As String = Nothing
 
+        Dim xml As XmlTextReader = New XmlTextReader(DNPatchConfig) With {
+            .WhitespaceHandling = WhitespaceHandling.None
+        }
+
+        xml.Read() ' XML declaration
+        xml.Read() ' Document
+        xml.Read() ' ChannelList 
+        xml.Read() ' Local
+
+        ' Skip-skip
+        xml.Read() ' Version
+        xml.Read() ' Update
+        xml.Read() ' Guidepage
+        xml.Read() ' Homepage
+
+        While Not xml.EOF
+            If Not xml.IsStartElement() Then Exit While ' Kalau tidak element pertama exit aja
+
+            xml.Read() ' Login tag
+
+            If (xml.HasAttributes) Then
+                ip &= xml.GetAttribute("addr") & ";"
+                port &= xml.GetAttribute("port") & ";"
+            End If
+        End While
+
+        Dim param As String = String.Format("/ip:{0} /port:{1} /language:{2}", ip.TrimEnd(";"), port.TrimEnd(";"), "ENG")
+        execFile(DNExe, param)
+        Threading.Thread.Sleep(2000)
+        Application.Exit()
     End Sub
 End Module
