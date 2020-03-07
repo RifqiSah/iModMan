41934fad094b8688b4f418d70392b01db13cb832
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-07 08:06:31 +0700

    fix: unable to load .NET managed dll
    Error if found 2 dll with same name or different version

--- iModMan/Modules/mdlMain.vb
+++ iModMan/Modules/mdlMain.vb
@@ -1,11 +1,12 @@
 ﻿Imports System.IO
 
 Module mdlMain
-    <Obsolete>
+
     Public Sub Main()
-        ' Load dulu DLL nya
-        AppDomain.CurrentDomain.AppendPrivatePath("iModMan")
+        ' Initialisi pertama
         LoadNativeDlls()
+        WriteLog("iModMan:Main", ErrorType.info, "==== Program Start ====")
+        LoadManagedDlls()
 
         ' Init style
         Application.EnableVisualStyles()
@@ -13,7 +14,7 @@ Module mdlMain
         ' Cek apakah aplikasi sudah berjalan sebelumnya
         If (IsAppAplreadyRunning()) Then
             MessageBox.Show(Form.ActiveForm, Application.ProductName & " sedang berjalan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Application.Exit()
+            ProgramEnd()
 
             Exit Sub
         End If
@@ -21,14 +22,17 @@ Module mdlMain
         ' Cek koneksi internetnya
         If (IsInternetAvailable() = False) Then
             MessageBox.Show(Form.ActiveForm, "Pastikan koneksi internet Anda tersedia sebelum menjalankan " & Application.ProductName & "!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Application.Exit()
+            ProgramEnd()
 
             Exit Sub
         End If
 
         ' Untuk commandline
         Dim Cmd() As String = Environment.GetCommandLineArgs
+
         If UBound(Cmd) > 0 Then ' Ada ?
+            WriteLog("iModMan:Main", ErrorType.debug, String.Format("Command line: {0}", Cmd(1)))
+
             Select Case Cmd(1).ToUpper
                 Case "/TOOLS"
                     frmTest.Show()
@@ -36,7 +40,6 @@ Module mdlMain
 
                 Case "/UPDATED"
                     frmMain.Show()
-                    ' doUpdateProg(frmMain)
                     Application.Run()
 
                 Case Else
@@ -44,9 +47,18 @@ Module mdlMain
 
             End Select
         Else ' Kosong ?
+            WriteLog("iModMan:Main", ErrorType.debug, "Attempting to check for updates")
+
             execFile(Path.Combine(Application.StartupPath, "iModMan\Updater.exe"), "/CHECK")
-            Application.Exit()
+            ProgramEnd()
         End If
+    End Sub
+
+    Public Sub ProgramEnd(Optional ByVal code As Integer = 0)
+        ' WriteLog("iModMan:Main", ErrorType.debug, String.Format("Program will terminated with error code {0}.", code))
+        WriteLog("iModMan:Main", ErrorType.info, "==== Program Terminated ====" & vbCrLf)
+
+        Application.Exit()
     End Sub
 
     Private Sub LoadNativeDlls()
@@ -58,4 +70,28 @@ Module mdlMain
         LoadWin32Library(Path.Combine(Application.StartupPath, "iModMan\iCrypto.dll"))
         LoadWin32Library(Path.Combine(Application.StartupPath, "iModMan\iDias.dll"))
     End Sub
+
+    Private Sub LoadManagedDlls()
+        Try
+            AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf DoResolve
+        Catch ex As Exception
+            WriteLog("iModMan:Main", ErrorType.errors, ex.Message)
+            MessageBox.Show(Form.ActiveForm, ex.Message, "Error", MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function DoResolve(ByVal sender As Object, ByVal e As ResolveEventArgs) As Reflection.Assembly
+        Dim handle As Reflection.Assembly = Nothing
+        Dim file As String = e.Name.Substring(0, InStr(e.Name, ",") - 1) & ".dll"
+
+        WriteLog("iModMan:LoadManagedDlls", 5, String.Format("- Resolving {0}", file))
+        Try
+            handle = Reflection.Assembly.LoadFrom(Path.Combine(Application.StartupPath, "iModMan", file))
+        Catch ex As Exception
+            WriteLog("iModMan:LoadManagedDlls", ErrorType.errors, ex.Message)
+            MessageBox.Show(Form.ActiveForm, ex.Message, "Error", MessageBoxIcon.Error)
+        End Try
+
+        Return handle
+    End Function
 End Module
