1e4a509eabc371e0ea2c2d81a1e0cb9556e8d16d
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-15 06:45:41 +0700

    add: batch env file copier

---
+++ iModMan/Forms/Tools/frmToolsEnv.vb
@@ -0,0 +1,31 @@
+﻿Imports System.IO
+
+Public Class frmToolsEnv
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        prosesEnv(TextBox1.Text)
+    End Sub
+
+    Private Sub prosesEnv(ByVal sFile As String)
+        If Not (File.Exists(sFile)) Then
+            MsgBox("Invalid file!", MsgBoxStyle.Critical)
+            Exit Sub
+        End If
+
+        Dim dPath = Path.GetDirectoryName(sFile) & "\"
+        Dim dFiles As String() = TextBox2.Text.Split(vbCrLf)
+
+        For Each dFile As String In dFiles
+            Dim sDesc As String = Path.Combine(dPath, dFile.Trim())
+            File.Copy(sFile, sDesc, True)
+        Next
+
+        MsgBox("Selesai!", MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim MyFiles() As String = e.Data.GetData(DataFormats.FileDrop)
+            TextBox1.Text = MyFiles(0)
+        End If
+    End Sub
+End Class
